9f6d3212bb4760b00bbc5ad9785ec54c0287245f
darri <iaroslag@metropolia.fi>
2022-09-12 09:53:04 +0300

    .

---
+++ sanakirja2.py
@@ -0,0 +1,7 @@
+työntekijät = {
+            "Viivi":('bulevardi', "050-1234567"),
+           "Ahmed":('Tehtaankatu', "040-1112223"),
+           "Pekka":('joku katu', "050-7654321")}
+tiedot = työntekijät['Viivi']
+print(tiedot[0])
+print(tiedot[1])
