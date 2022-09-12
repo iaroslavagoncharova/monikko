9e4928a5f85f33a8d23b71c0bf6bdf4e178c983c
darri <iaroslag@metropolia.fi>
2022-09-12 09:31:25 +0300

    .

---
+++ joukko.py
@@ -0,0 +1,14 @@
+pelit = {"Monopoli", "Shakki", "Cluedo"}
+print(pelit)
+
+pelit.add("Dominion")
+print(pelit)
+
+pelit.remove("Shakki")
+print(pelit)
+
+pelit.add("Cluedo")
+print(pelit)
+
+for p in pelit:
+    print(p)
